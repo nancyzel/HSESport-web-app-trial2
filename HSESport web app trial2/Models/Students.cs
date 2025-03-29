09a44f9f06d6ea52f0dc176f9bc602c5708daa41
Nastya Zelenina <anastasiazelenina346@gmail.com>
2025-03-29 18:54:08 +0500

    data base head

--- HSESport web app trial2/Models/Students.cs
+++ HSESport web app trial2/Models/Students.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HSESport_web_app_trial2.Models
 {
     public class Students
     {
         [Key]
+        [Display(Name = "Номер зачетной книжки студента")]
         /// <summary>
         /// номер зачетки студента
         /// </summary>
@@ -13,6 +15,7 @@ namespace HSESport_web_app_trial2.Models
             get; set;
         }
 
+        [Display(Name = "Имя студента")]
         /// <summary>
         /// имя студента
         /// </summary>
@@ -21,6 +24,7 @@ namespace HSESport_web_app_trial2.Models
             get; set;
         }
 
+        [Display(Name = "Фамилия студента")]
         /// <summary>
         /// фамилия студента
         /// </summary>
@@ -29,14 +33,16 @@ namespace HSESport_web_app_trial2.Models
             get; set;
         }
 
+        [Display(Name = "Отчество студента")]
         /// <summary>
         /// отчество студента
         /// </summary>
-        public string? studentSecondName
+        public string? StudentSecondName
         {
             get; set;
         }
 
+        [Display(Name = "Корпоративная почта студента")]
         /// <summary>
         /// корпоративная электронная почта студента
         /// </summary>
@@ -45,6 +51,7 @@ namespace HSESport_web_app_trial2.Models
             get; set;
         }
 
+        [Display(Name = "Число посещений студентом спортивных секций")]
         /// <summary>
         /// число посещений студентом спортивных секций
         /// </summary>
